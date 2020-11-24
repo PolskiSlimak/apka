f012d53863acccca97d829c664742ac3428793ea
KWA <slimak411@wp.pl>
2020-11-24 16:05:37 +0100

    dodanieDone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   todos = [];
-
+  done = [];
   onAdd(event) {
-    this.todos.push(event.value);
-    event.value = '';
+    let value = event.value;
+    if (value != "")
+      this.todos.push(value);
+    value = '';
+  }
+  addToDone(event) {
+    let todos = this.todos;
+    let i = todos.indexOf(event);
+    todos.splice(i,1);
+    this.done.push(event);
   }
   onClear() {
     this.todos = [];
+    this.done = [];
   }
 }
