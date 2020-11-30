b29abf111e5c6bcffa7fff0f9de43e29847a9db8
KWA <slimak411@wp.pl>
2020-11-30 17:18:45 +0100

    obsługa localStorage

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -5,37 +5,45 @@ import { of } from "rxjs";
 })
 export class TodoService {
 
-  private todos = [
-    "a", "b", "c"
-  ];
+  private todos = [];
   private done = [];
 
   constructor() {}
 
   getTodos() {
+    this.todos = JSON.parse(localStorage.getItem('todos')) ?? [];
     return of(this.todos);
   }
 
   getDone() {
+    this.done = JSON.parse(localStorage.getItem('done')) ?? [];
     return of(this.done);
   }
 
   addTodo(todo: string) {
     this.todos.push(todo);
+    this.setLocalStorage('todos', this.todos);
   }
 
   addDone(done: string) {
     this.done.push(done);
+    this.setLocalStorage('done', this.done);
   }
 
   removeAll() {
     this.todos.splice(0, this.todos.length);
     this.done.splice(0, this.done.length);
+    this.setLocalStorage('todos', this.todos);
+    this.setLocalStorage('done', this.done);
   }
 
   removeItem(item) {
     let todos = this.todos;
     let i = todos.indexOf(item);
     todos.splice(i,1);
+    this.setLocalStorage('todos', this.todos);
+  }
+  setLocalStorage(listType, list) {
+    localStorage.setItem(listType, JSON.stringify(list));
   }
 }
